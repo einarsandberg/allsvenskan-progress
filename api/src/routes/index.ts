c26ac58f72e88367c43d24c098e6ff3ecc9fdcbc
Einar Sandberg <einars92@gmail.com>
2020-01-23 12:23:36 +0100

    send matches to client

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-
+import MatchesRouter from './matches';
 // Init router and path
 const router = Router();
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
 });
+
+router.use('/matches', MatchesRouter);
 // Export the base-router
 export default router;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
+export interface Match {
+  homeTeam: string;
+  awayTeam: string;
+  homeGoals: string;
+  awayGoals: string;
+}
 
 const App: React.FC = () => {
+  const [matches, setMatches] = useState<Match[]>([]);
+  useEffect(() => {
+    fetch('/api/matches')
+      .then((res: Response) => res.json())
+      .then((data: Match[]) => {
+        setMatches(data);
+      });
+  });
   return (
     <div className="App">
     </div>
   );
-}
+};
 
 export default App;
